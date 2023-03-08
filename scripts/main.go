927041f361312308e2349dd1168e6c17b2eadd03
ofadiman <ofadiman@gmail.com>
2023-03-08 22:59:40 +0100

    feat: handle inputs and outputs in golang script

--- scripts/main.go
+++ scripts/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
 	fmt.Println("golang script executed")
+
+	input := os.Getenv("INPUT_SCRIPT")
+	fmt.Printf("selected file: %s\n", input)
+
+	githubOutput := os.Getenv("GITHUB_OUTPUT")
+	file, err := os.OpenFile(githubOutput, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	if _, err := file.WriteString("value=foo\n"); err != nil {
+		log.Println(err)
+	}
+
+	err = file.Close()
+	if err != nil {
+		panic(err)
+	}
 }
